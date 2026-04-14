fe263970afe1870f8e080754cd9309cbd16c3965
Thomaswalsh1 <130932404+thomaswalsh1@users.noreply.github.com.>
2026-04-14 16:17:57 -0400

    Approaching better environment for react testing

--- examples/react-app-example/src/App.tsx
+++ examples/react-app-example/src/App.tsx
@@ -1,11 +1,42 @@
 import React from "react";
 import { Terminal, useTerminal } from "termite-react";
-import "./App.css"
+import "./App.css";
+import type { TerminalState } from "termite-core";
+
+class TerminalObject {
+  state: TerminalState;
+  onInput: (value: string) => void;
+  onSubmit: () => Promise<void>;
+}
 
 export default function App() {
-  const { state, onInput, onSubmit } = useTerminal(async (cmd) => {
-    return `you ran: ${cmd}`;
-  }, {cwd: "/home/thomas", prompt: "$"});
+  const { state, onInput, onSubmit } = useTerminal(
+    async (cmd) => {
+      return `you ran: ${cmd}`;
+    },
+    { cwd: "/home/thomas", prompt: "$" },
+  );
+
+  const terminal2: TerminalObject = useTerminal(
+    async (cmd) => {
+      return `you ran: ${cmd}`;
+    },
+    { cwd: "/home/thomas", prompt: "$" },
+  );
+
+  const terminal3: TerminalObject = useTerminal(
+    async (cmd) => {
+      return `you ran: ${cmd}`;
+    },
+    { cwd: "/home/thomas", prompt: "$" },
+  );
+
+  const terminal4: TerminalObject = useTerminal(
+    async (cmd) => {
+      return `you ran: ${cmd}`;
+    },
+    { cwd: "/home/thomas", prompt: "$" },
+  );
 
   return (
     <div className="main-container" id="main-screen">
@@ -21,10 +52,28 @@ export default function App() {
             input: "terminal-input",
             command: "terminal-command",
           }}
+          className="overall"
           state={state}
           onInput={onInput}
           onSubmit={onSubmit}
         />
+      </div>
+      <div id="expanding-system">
+        <div id="row-1">
+          <Terminal
+            className="terminal-2"
+            state={terminal2.state}
+            onInput={terminal2.onInput}
+            onSubmit={terminal2.onSubmit}
+          />
+          <Terminal
+            className="terminal-3"
+            state={terminal3.state}
+            onInput={terminal3.onInput}
+            onSubmit={terminal3.onSubmit}
+          />
+        </div>
+        <div id="row-2"></div>
       </div>
     </div>
   );
